4288a6aa8137c80b96f759bbd4370aeab904a346
Your Name <you@example.com>
2023-06-08 10:10:18 +0700

    Update Navigation

--- js/home.js
+++ js/home.js
@@ -12,3 +12,26 @@ setInterval(() => {
 
 
 }, 0);
+
+
+function ActiveButton(index) {
+    if (index == 1) {
+        document.getElementById("Nav1").classList.add("navactive")
+        document.getElementById("Nav2").classList.remove("navactive")
+        document.getElementById("Nav3").classList.remove("navactive")
+    }
+
+    if (index == 2) {
+        document.getElementById("Nav2").classList.add("navactive")
+        document.getElementById("Nav1").classList.remove("navactive")
+        document.getElementById("Nav3").classList.remove("navactive")
+    }
+
+    if (index == 3) {
+        document.getElementById("Nav3").classList.add("navactive")
+        document.getElementById("Nav2").classList.remove("navactive")
+        document.getElementById("Nav1").classList.remove("navactive")
+    }
+
+
+}
